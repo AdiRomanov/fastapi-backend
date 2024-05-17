f5d33e356b5fa28a9efc2772946e3ee8266ad502
AdiRomanov <romanovadi44@gmail.com>
2024-05-17 21:31:52 +0300

    update

--- app/routers/ingredient.py
+++ app/routers/ingredient.py
@@ -19,6 +19,11 @@ def get_ingredients(db: Session = Depends(get_db), current_user: int = Depends(o
         models.Ingredient.ingredient.contains(search)).limit(limit).offset(skip).all()
     return ingredients
 
+@router.get("/ingredient-by-name/{name}", response_model=schemas.Ingredient)
+def get_ingredient_by_name(name: str, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    ingredient = db.query(models.Ingredient).filter(models.Ingredient.ingredient == name).first()
+    return ingredient
+
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Ingredient)
 def create_ingredient(ingredient: schemas.IngredientBase, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     new_ingredient = models.Ingredient(**ingredient.dict())
@@ -28,16 +33,19 @@ def create_ingredient(ingredient: schemas.IngredientBase, db: Session = Depends(
 
     return new_ingredient
 
+
 @router.get("/{id}", response_model=schemas.Ingredient)
-def get_ingredient(id: int, db: Session = Depends(get_db)):
+def get_ingredient(id: int, db: Session = Depends(get_db)   ):
     ingredient = db.query(models.Ingredient).filter(models.Ingredient.id == id).first()
     return ingredient
+
 
 @router.put("/{id}", response_model=schemas.Ingredient)
 def update_ingredient(id: int, ingredient: schemas.IngredientBase, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     db.query(models.Ingredient).filter(models.Ingredient.id == id).update(ingredient.dict())
     db.commit()
     return db.query(models.Ingredient).filter(models.Ingredient.id == id).first()
+
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_ingredient(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
--- app/routers/shopping_list.py
+++ app/routers/shopping_list.py
@@ -17,18 +17,26 @@ def get_shopping_list(db: Session = Depends(get_db), current_user: int = Depends
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ShoppingList)
 def create_shopping_list(shopping_list: schemas.ShoppingListCreate, current_user: int = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+    entity = db.query(models.UserIngredient).filter(
+        models.ShoppingList.user_id == current_user.id,
+        models.ShoppingList.ingredient_id == shopping_list.ingredient_id
+    ).first()
+    
+    if entity:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Ingredient already exists for this user.")
+
     new_shopping_list = models.ShoppingList(**shopping_list.dict())
     db.add(new_shopping_list)
     db.commit()
     db.refresh(new_shopping_list)
-
+ 
     return new_shopping_list
 
 
-@router.get('/{id}', response_model=schemas.ShoppingList)
+@router.get('/{id}', response_model=List[schemas.ShoppingList])
 def get_shopping_list_by_id(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user) ):
 
-    shopping_list = db.query(models.ShoppingList).filter(models.ShoppingList.id == id).first()
+    shopping_list = db.query(models.ShoppingList).filter(models.ShoppingList.user_id == current_user.id).all()
     if not shopping_list:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Shopping List with id: {id} does not exist")
@@ -45,6 +53,7 @@ def update_shopping_list(id: int, shopping_list: schemas.ShoppingListUpdate, db:
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_shopping_list(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    db.query(models.ShoppingList).filter(models.ShoppingList.id == id).delete(synchronize_session=False)
+    db.query(models.ShoppingList).filter(models.ShoppingList.user_id == current_user.id,
+                                         models.ShoppingList.ingredient_id == id).delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
